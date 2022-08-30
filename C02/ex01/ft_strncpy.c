d5817d2ee42370de932972d931f65908b0792f86
Robin Gremion (42) <rgremion@student.42lausanne.ch>
2022-08-30 23:24:15 +0200

    Add files via upload

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robingremion <robingremion@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 22:42:34 by robingremio       #+#    #+#             */
+/*   Updated: 2022/08/30 22:48:53 by robingremio      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i ++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i ++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
