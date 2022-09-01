28eb3a358a1caafe0d17e3a01b3e46848a1c1c52
Robin Gremion (42) <rgremion@student.42lausanne.ch>
2022-09-01 21:14:54 +0200

    Add files via upload

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: robingremion <robingremion@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/01 20:59:51 by robingremio       #+#    #+#             */
+/*   Updated: 2022/09/01 21:12:17 by robingremio      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr (nb / 10);
+		nb = nb % 10;
+	}
+	ft_putchar(nb + 48);
+}
